0dce3a21d704c0a2a205a396ab4f0bf1d4239727
Carl Henderson <chuckhendo@gmail.com>
2018-12-31 09:05:53 -0500

    updates to save leaderboard to file

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const {promisify} = require("util");
+const fs = require("fs");
 const uuid = require("uuid").v4;
 const express = require("express");
+
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
 
 const app = express();
 
 const openRequests = {};
-const leaderboard = [];
 
 app.get('/call/:username', (req, res) => {
     const id = uuid();
@@ -13,7 +17,8 @@ app.get('/call/:username', (req, res) => {
     res.send(id);
 });
 
-app.get(`/leaderboard`, (req, res) => {
+app.get(`/leaderboard`, async (req, res) => {
+    const leaderboard = JSON.parse(await readFile("./leaderboard.json"));
     const sortedLeaderboard = leaderboard.sort((a, b) => {
         if(a.time[0] === b.time[0]) {
             return a.time[1] - b.time[1];
@@ -24,21 +29,25 @@ app.get(`/leaderboard`, (req, res) => {
     res.send(`
         <ol>
             ${sortedLeaderboard.map((item) => {
-                return `<li>${item.username}: ${item.time[0]}.${item.time[1]}`;
+                return `<li>${item.username}: ${item.time[0]}.${item.time[1].toString().padStart(9, '0')}`;
             }).join("\n")}
         </ol>
     `);
 });
 
-app.get(`/:id`, (req, res) => {
+app.get(`/:id`, async (req, res) => {
     const id = req.params.id;
     const initTime = openRequests[id].time;
     const username = openRequests[id].username;
     const diffTime = process.hrtime(initTime);
+    const leaderboard = JSON.parse(await readFile("./leaderboard.json"));
+    
     leaderboard.push({
         username,
         time: diffTime
     });
+
+    await writeFile("./leaderboard.json", JSON.stringify(leaderboard));
     res.send({ time: diffTime, username });
 });
 
